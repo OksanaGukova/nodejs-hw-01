223ea9909d0b429cabbbf38f3f7d20f029cb2f94
OksanaGukova <kseniagukova@ukr.net>
2024-08-15 23:01:33 +0300

    Update removeAllContacts.js

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, '[]');
+    console.log('All contacts have been removed successfully!');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
 removeAllContacts();
